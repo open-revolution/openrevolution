f5b275bc4777313cb0553f15ed72ab04438f134c
tanvirchahal <tnvr009@gmail.com>
2018-06-01 02:59:43 +0530

    Updated the homepage

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,26 @@ $(document).on('click', 'a[href^="#"]', function (event) {
   }
 });
 
+//Modals
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
 $(window).resize(function() {
   var more = document.getElementById("js-navigation-more");
   if ($(more).length > 0) {
