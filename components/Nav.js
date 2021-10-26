21e04b0a898de6ea453ef0715284e674a11fcb5b
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-26 09:55:57 +0200

    [theme/nav,#1][s]: navbar reproduced in nextjs using config approach.
    * Can't reproduce the dropdown on FAQ but don't think that matters much yet

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { PlusSmIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 
 import siteConfig from '../config/siteConfig.js'
@@ -51,6 +52,17 @@ export default function Nav() {
                       </a>
                       </Link>
                     ))}
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <button
+                    type="button"
+                    className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-yellow-300 shadow-sm hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <PlusSmIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                    <span>Get the Book Now</span>
+                  </button>
                 </div>
               </div>
             </div>
--- config/navLinks.js
+++ config/navLinks.js
@@ -1,5 +1,12 @@
 const navLinks = [
-  { href: '/about', name: 'About' },
+  { href: '/news/', name: 'News' },
+  { href: '/pay-what-feels-right/', name: 'Pay What Feels Right' },
+  { href: '/press/', name: 'Press' },
+  { href: '/events/', name: 'Events' },
+  { href: '/get-involved/', name: 'Get Involved' },
+  { href: '/faq/', name: 'FAQ' },
+  { href: '/preview/', name: 'Preview' },
+  { href: '/contact/', name: 'Contact' },
 ]
 
 export default navLinks
