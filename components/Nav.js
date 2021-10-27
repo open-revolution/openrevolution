ff8b39e58ec96e77eca5fdd6b7e70a87c0a6ed01
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-27 23:10:26 +0200

    [#1,theme/nav][s]: get this book button actually links and works.

--- components/Nav.js
+++ components/Nav.js
@@ -56,13 +56,15 @@ export default function Nav() {
               </div>
               <div className="flex items-center">
                 <div className="flex-shrink-0">
-                  <button
-                    type="button"
-                    className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-yellow-300 shadow-sm hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
+                  <Link href="/#get-book">
+                    <a
+                      type="button"
+                      className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-yellow-300 shadow-sm hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
                     <PlusSmIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-                    <span>Get the Book Now</span>
-                  </button>
+                    <span>Get the Book</span>
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
