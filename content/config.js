412ced0cfb74eafaa75cf7cab87e2c897ff7264b
Rufus Pollock <rufus.pollock@datopian.com>
2023-12-28 21:39:34 +0100

    [config][s]: revert site title to Open Revolution and correct navbar label.

--- content/config.js
+++ content/config.js
@@ -1,12 +1,17 @@
 const config = {
+  title: "Open Revolution",
+  tagline: "Rewriting the rules of the information age",
+  description:
+    "Forget everything you know about the digital age. It’s not about privacy, surveillance, AI or blockchain—it’s about ownership. Because, in a digital age, who owns information owns the future.",
+  author: "Rufus Pollock",
   navLinks: [
     { href: "/news/", name: "News" },
     { href: "/events/", name: "Events" },
-    { href: "/make-it-happen/", name: "Get Involved" },
     { href: "/faq/", name: "FAQ" },
     { href: "/preview/", name: "Preview" },
-    { href: "/diginomics/", name: "Platforms" },
-    { href: "/platformnomics/", name: "Platforms" },
+    { href: "/diginomics/", name: "Diginomics" },
+    { href: "/platformnomics/", name: "Platformnomics" },
+    { href: "/make-it-happen/", name: "Get Involved" },
     { href: "/contact/", name: "Contact" },
   ],
   analytics: 'G-SZTB5WW5H4',
